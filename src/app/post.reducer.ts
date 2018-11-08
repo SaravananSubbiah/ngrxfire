1257975076be33c108b0409b13c4a1c7a9c2a6f7
SaravananSubbiah <abini.sara2001@gmail.com>
2018-11-08 15:31:25 -0500

    Reducer changed

--- src/app/post.reducer.ts
+++ src/app/post.reducer.ts
@@ -4,32 +4,34 @@ import { Post } from './post';
 export type Action = PostActions.All;
 
 // Default app state;
-const defaultState: Post = {text: 'Hello! I am the default postt',
-likes: 0}
+const defaultState: Post = {
+    text: 'Hello! I am the default postt',
+    likes: 0
+}
 
 
 // Helpler function to create new state object;
 
-const newState = (state, newData) =>{
+const newState = (state, newData) => {
     return Object.assign({}, state, newData);
 
 }
 
 export function postReducer(state: Post = defaultState, action: Action) {
-    console.log(action.type,' , ', state);
+    console.log(action.type, ' , ', state);
 
-    switch(action.type){
+    switch (action.type) {
         case PostActions.EDIT_TEXT:
             console.log(action.type);
-            return newState(state, {text: action.payload});
-        case PostActions.UPVOTE: 
-        console.log(action.type);
+            return newState(state, { text: action.payload });
+        case PostActions.UPVOTE:
+            console.log(action.type);
             return newState(state, { likes: state.likes + 1 });
-            case PostActions.DOWNVOTE: 
-        console.log(action.type);
-            return newState(state,  {likes: state.likes -1 });
-        default : 
-        console.log(action.type);
+        case PostActions.DOWNVOTE:
+            console.log(action.type);
+            return newState(state, { likes: state.likes - 1 });
+        default:
+            console.log(action.type);
             return defaultState;
     }
 }
